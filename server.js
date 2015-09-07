096ac0aa363473887f1612e2d93be68878f679f7
willworks <scnuzhonghanjin@gmail.com>
2015-09-07 19:19:07 +0800

    ajax POST请求部分完成

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express'),//express路由
 	fs = require('fs'),// node文件操作板块
-    app = express();//实例化
+    app = express(),//实例化
+    bodyParser = require('body-parser');
 
 app.use('/', express.static(__dirname + '/app'));
+// see https://github.com/expressjs/body-parser
+// 添加 body-parser ，能起到分析http报文的作用，自动划分出每一个传递的参数
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.get('/ajax', function(req, res){
+
+app.get('/ajax_get', function(req, res){
 	fs.readFile("data.json",'utf-8',function(err,data){  
 	    if(err){  
 	        console.log("error");  
@@ -13,6 +19,23 @@ app.get('/ajax', function(req, res){
 	        console.log(data);  
 	    }  
 	});  
+});
+
+
+app.post('/ajax_post', function(req, res){
+	var name=req.body.name,
+		age=req.body.age,
+		sex=req.body.age;
+
+	console.log(name);
+	console.log(age);
+	console.log(sex);
+
+	fs.open('data.json', 'r', function(err, fd) {
+	    // got fd file descriptor
+	});
+ 
+	res.send('{"info":"success"}');
 });
 
 app.listen(8080, function(){
