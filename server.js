3a1d84874973aaa9da19a990ac9f0e4aabc7c533
willworks <scnuzhonghanjin@gmail.com>
2015-09-15 17:29:07 +0800

    node url传参处理

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),//express路由
 	fs = require('fs'),// node文件操作板块
     app = express(),//实例化
-    bodyParser = require('body-parser');//http报文文本解析
+    bodyParser = require('body-parser'),//http报文文本解析
+    url = require('url');//url处理模块
 
 // 路由中转静态资源
 app.use('/', express.static(__dirname + '/app'));
@@ -11,17 +12,35 @@ app.use('/', express.static(__dirname + '/app'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+
+
 // 处理GET请求
 app.get('/ajax_get', function(req, res){
+	/**
+	 * node url模块获取url传参
+	 * url.parse(req.url, true).query //参数保存为JSON格式
+	 * url.parse(req.url).query //参数保存为字符串
+	 */
+	var query = url.parse(req.url, true).query;
+	console.log(query.name);
 	fs.readFile("data.json",'utf-8',function(err,data){  
 	    if(err){  
 	        console.log("error");  
 	    }else{  
-	    	res.send(data);
-	        console.log(data);  
+	    	var name = JSON.parse(data).name; 
+	    	console.log(name);
+	    	if(query.name == name){
+	    		res.send(data);
+	    		console.log(data);  
+	    	}else{
+	    		res.send('{"info":"查询信息不存在！"}');
+	    		console.log("查询信息不存在！"); 
+	    	}
 	    }  
 	});  
 });
+
+
 
 // 处理JSONP请求
 app.get('/jsonp', function(req, res){ 
@@ -36,6 +55,8 @@ app.get('/jsonp', function(req, res){
 	    }  
 	}); 
 });
+
+
 
 // 处理POST请求
 app.post('/ajax_post', function(req, res){
@@ -56,6 +77,8 @@ app.post('/ajax_post', function(req, res){
 	    }  
 	});  
 });
+
+
 
 app.listen(8080, function(){
 	console.log('listening on *:8080');
